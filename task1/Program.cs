410a8b2e0cd0c1be72cdb69b285e5fe2036f6010
Nikita Skripnikov <dr-proroc2017@yandex.ru>
2023-01-31 12:36:49 +0300

    git add task1

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// dotnet -проверка, все ли установленно
+// dotnet new console - создание проэкта
+// dotnet run - запуск проекта
+
+Console.Write("Ввеите целое число: ");// вывод в консоль сообщение
+int number = Convert.ToInt32(Console.ReadLine());// int.Parse() или Convert.ToInt32()
+int sqr = number * number;
+System.Console.WriteLine($"Квадрат числа: {sqr} ");// Упращенный вариант в начале пишем ($"Текст: {});
+// System.Console.WriteLine("Квадрат числа: " + sqr); Обычный вариант
