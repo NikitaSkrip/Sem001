c39039e32cef781d82ccc3c42ff6233a0f5001f4
Nikita Skripnikov <dr-proroc2017@yandex.ru>
2023-02-03 22:10:19 +0300

    git add task2.1 removing the second number

---
+++ task2.1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу которая будет принемать на входе два числа и выводить является ли второе число кратное первому.
+// Если число 2 не кратное число 1, то программа остаток от деления.
+// 34,5 остаток 4
+// 16,4 кратно
+
+
+System.Console.WriteLine("Введите первое число: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+int remainder = ( firstnumber % secondnumber );
+
+if (remainder == 0)
+{
+    System.Console.WriteLine($"Первое число кратное второму числу {remainder}");
+}
+else
+{
+    System.Console.WriteLine($"Не кратно, остаток  { remainder }");
+}
+
