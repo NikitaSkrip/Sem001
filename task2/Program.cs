01b8545637b08fa6973b086bc9f0fedd9d51163b
Nikita Skripnikov <dr-proroc2017@yandex.ru>
2023-02-03 14:15:56 +0300

    git add task2 random

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает 
+// наибольшую цыфру числа.
+// 78->8
+// 12->2
+// 85->8
+
+Random rand = new Random();// Обьявление обьекта класса рандом
+int randNumber = rand.Next(100, 1000);//.Next генерирует числа от (10 до 99) или больше
+System.Console.WriteLine(randNumber);
+
+if (randNumber/100 > randNumber%100)
+{
+    System.Console.WriteLine($"Большая цифра числа {randNumber}: { randNumber/100 }");
+}
+else
+{
+    System.Console.WriteLine($"Большая цифра числа {randNumber}: { randNumber%100 }");
+}
+
