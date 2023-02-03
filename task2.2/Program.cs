f7e412bfe3342a7e75d3afe5d0b26c0eb1e5a2f7
Nikita Skripnikov <dr-proroc2017@yandex.ru>
2023-02-03 14:32:42 +0300

    git add task2.2 remove second digit

---
+++ task2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите программу,которая выводит любое трехзначное число, и удаляет вторую цифру этого числа.
+
+// пример
+// 456->46
+// 782->72
+// 918->98
+
+Random rand = new Random();// Обьявление обьекта класса рандом
+int randNumber = rand.Next(100, 1000);//.Next генерирует числа от (10 до 99) или больше
+System.Console.WriteLine(randNumber);
+
+System.Console.WriteLine($"Удаление второй цифры данного числа {randNumber}: { randNumber/100 *10 + randNumber%10 }");
